8b42e5494923ca6f16765a2cfbed3e5c52b125ab
Thibault Jaime <jaime.thibault@gmail.com>
2019-06-04 11:24:46 +0200

    search ok, with call to Giphy API

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "chbjWu4ljDcEjyrWDN"
+    }
+  }
+
+  search = (query) => {
+    // return a list of gifs and update the state of the App
+    giphy("wALTMYblLBjK3XIHeNwbPZtQd15pMXRj").search({
+        q: query,
+        rating: 'g',
+        limit: 10
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
 
   render() {
-    const gifs = [
-      { id: "chbjWu4ljDcEjyrWDN" },
-      { id: "TLmMAirE93GeBmXsfA" }
-    ]
-
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchF={this.search} />
           <div className="selected-gif">
-            <Gif id="chbjWu4ljDcEjyrWDN" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
           <div className="gif-list">
-            <GifList gifs={gifs} />
+            <GifList gifs={this.state.gifs} />
           </div>
         </div>
       </div>
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+  handleUpdate = (event) => {
+    this.props.searchF(event.target.value);
+  }
+
   render() {
     return (
-      <input type="text" className="form-control form-search"/>
+      <input type="text" className="form-control form-search" onChange={this.handleUpdate}/>
     )
   }
 }
